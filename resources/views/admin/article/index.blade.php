1382887d15923d507811d3ac5f7aa0d93c10b72f
paleo_engineer <g843522r@gmail.com>
2020-09-11 08:40:20 +0900

    #11_change_to_card_layout

--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -8,62 +8,24 @@
 @section('content')
   <div class="container">
     <div class="row">
-      <h2>投稿記事一覧画面</h2>
+      <h2>投稿記事一覧</h2>
     </div>
+
     <div class="row">
-      {{-- 新規投稿ボタン --}}
+    {{-- 投稿作成ボタン --}}
       <div class="col-md-4">
-        {{-- <a href="{{ action('Admin\ArticleController@add') }}" role="button" class="btn btn-primary">新規作成</a> --}}
-        <a class="btn btn-primary js-modal-open" href="" data-target="modal01">新規投稿する</a>
-        {{-- <button class="btn btn-primary js-modal-open" roll="button" data-target="#modal01">新規投稿</button> --}}
-      </div>
-
-      {{-- モーダルウィンドウ --}}
-      <div id="modal01" class="c-modal js-modal">
-        <!-- 背景 -->
-        <div class="c-modal_bg js-modal-close">
-        </div>
-        <!-- 本体 -->
-        <div class="c-modal_content">
-          <div class="c-modal_content_inner">
-            <h2>投稿の新規作成画面</h2>
-            <form action="{{ action('Admin\ArticleController@create') }}" method="post" enctype="multipart/form-data" id="post-form">
-              <!-- バリデーションメッセージを表示 -->
-              @error('body')
-                <tr><td>{{$message}}</td></tr>
-              @enderror
-              <!-- 投稿本文 -->
-              <div class="form-group row">
-                <label class="col-md-2" for="body">投稿内容</label>
-                <div class="col-md-10">
-                  {{-- <textarea class="form-control" name="body" cols="30" rows="6">{{ old('body') }}</textarea> --}}
-                  <textarea name="body" cols="30" rows="6">{{ old('body') }}</textarea>
-                </div>
-              </div>
-              <!-- 画像の投稿 -->
-              <div class="form-group row">
-                <label class="col-md-2" for="body">画像</label>
-                <div class="col-md-10">
-                  <input type="file" class="form-control-file" name="image">
-                </div>
-              </div>
-
-              {{ csrf_field() }}
-              <input type="submit" class="btn btn-primary" value="投稿する">
-            </form>
-            <a class="js-modal-close c-modal_close" href=""><span>閉じる</span></a>
-          </div>
-        </div>
+        <a class="btn btn-primary js-modal-open" href="" data-target="modal01">＋投稿作成</a>
       </div>
 
       {{-- 投稿の検索 --}}
-      <div class="col-md-8">
-        <form action="{{ action('Admin\ArticleController@index') }}" method="get">
+      <div class="col-md-6 offset-md-2">
+        <label for="serch">キーワードで投稿を検索</label>
+        <form action="{{ action('Admin\ArticleController@index') }}" method="get" id="serch">
           <div class="form-group row">
-            <div class="col-md-8">
+            <div class="col-md-4">
               <input type="text" class="form-controll" name="cond_title" value="{{ $cond_title }}">
             </div>
-            <div class="col-md-2">
+            <div class="col-md-4">
               {{ csrf_field() }}
               <input type="submit" class="btn btn-primary" value="検索">
             </div>
@@ -72,38 +34,61 @@
       </div>
     </div>
 
-    {{-- 検索で取得した結果をforeachで表示する --}}
+    {{-- 投稿一覧 --}}
+    {{-- 検索で取得した結果をforeachでカード表示する --}}
     <div class="row">
-      <div class="list-news col-md-12 mx-auto">
-        <div class="row">
-          <table class="table table-dark">
-            <thead>
-              <tr>
-                <th width="10%">投稿ID</th>
-                <th width="10%">投稿者</th>
-                <th width="40%">本文</th>
-                <th width="20%">操作</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($articles as $post)
-                <tr>
-                  <th>{{ $post->id }}</th>
-                  <th>{{ $post->user_name }}</th>
-                  <td>{{ $post->body }}</td>
-                  <td>
-                    <div>
-                    <a href="{{ action('Admin\ArticleController@edit', ['id' => $post->id]) }}">編集する</a>
-                    </div>
-                    <div>
-                    <a href="{{ action('Admin\ArticleController@delete', ['id' => $post->id]) }}">削除する</a>
-                    </div>
-                  </td>
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
+      <div class="list-news col-md-10 mx-auto">
+        @foreach ($articles as $post)
+        <div class="card" style="max-width: 50em">
+          <div class="card-header bg-dark text-white">
+            投稿者：{{ $post->user_name }}
+          </div>
+          <div class="card-body">
+            {{ $post->body }}
+          </div>
+          <div class="card-footer">
+            <a href="{{ action('Admin\ArticleController@edit', ['id' => $post->id]) }}">編集する</a>
+            <a href="{{ action('Admin\ArticleController@delete', ['id' => $post->id]) }}">削除する</a>
+          </div>
         </div>
+        @endforeach
+      </div>
+    </div>
+  </div>
+
+  {{-- 投稿のモーダルウィンドウ --}}
+  <div id="modal01" class="c-modal js-modal">
+    <!-- 背景 -->
+    <div class="c-modal_bg js-modal-close">
+    </div>
+    <!-- 本体 -->
+    <div class="c-modal_content" >
+      <div class="c-modal_content_inner">
+        <h3>投稿を新規作成</h3>
+        <form action="{{ action('Admin\ArticleController@create') }}" method="post" enctype="multipart/form-data" id="post-form">
+          <!-- バリデーションメッセージを表示したい.. -->
+          @error('body')
+            <tr><td>{{$message}}</td></tr>
+          @enderror
+          <!-- 投稿本文 -->
+          <div class="form-group row">
+            <label class="col-md-2" for="body">投稿内容</label>
+            <div class="col-md-10">
+              <textarea name="body" cols="40" rows="5">{{ old('body') }}</textarea>
+            </div>
+          </div>
+          <!-- 画像の投稿 -->
+          <div class="form-group row">
+            <label class="col-md-2" for="body">画像</label>
+            <div class="col-md-10">
+              <input type="file" class="form-control-file" name="image">
+            </div>
+          </div>
+
+          {{ csrf_field() }}
+          <input type="submit" class="btn btn-primary" value="投稿する">
+        </form>
+        <a class="js-modal-close c-modal_close" href=""><span>閉じる</span></a>
       </div>
     </div>
   </div>

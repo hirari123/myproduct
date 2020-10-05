b563465f0be83366aae9937dce4ea96a321518be
paleo_engineer <g843522r@gmail.com>
2020-10-05 23:06:35 +0900

    #32 バリデーションメッセージ等の日本語化を実施

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('messages.Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    {{ __('messages.You are logged in!') }}
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>

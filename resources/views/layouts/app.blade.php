afd073911af9f500f8324a43dca9a938919d840f
paleo_engineer <g843522r@gmail.com>
2020-10-25 18:20:15 +0900

    #64 プロフィール編集のバグを修正した

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,8 @@
 
                             {{-- プロフィール編集 --}}
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('/admin/profile/edit') }}">プロフィール編集</a>
+                                <a class="dropdown-item"
+                                    href="{{ action('Admin\ProfileController@edit', ['id' => Auth::user()->id]) }}">プロフィール編集</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();document.getElementById('logout-form').submit()">
                                     {{ __('Logout') }}

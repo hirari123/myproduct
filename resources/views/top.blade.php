b7e56e9598b925041023482f14269b89abf36f93
paleo_engineer <g843522r@gmail.com>
2020-11-17 12:39:46 +0900

    #99 トップ画像の背景色や余白と等のデザインを調整

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,46 +2,48 @@
 
 {{-- メインコンテンツ --}}
 @section('content')
-<div class="top-contents">
-    <div class="top-wrapper" style="color:#fafafa">
-        <h1>
-            <div class="row">
-                <div class="mx-auto mt-3">
-                    パレオダイエットを
+<div class="top-contents my-0">
+    <div class="top-wrapper my-0" style="color:#fafafa">
+        <div class="bg-opacity">
+            <h1>
+                <div class="row">
+                    <div class="mx-auto mt-5">
+                        パレオダイエットを
+                    </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="mx-auto">
+                        コミュニティの力で<span style="color:#ffddaa; font-style: italic;">ブースト</span>させよう
+                    </div>
+                </div>
+            </h1>
+            <br>
             <div class="row">
                 <div class="mx-auto">
-                    コミュニティの力で<span style="color:#ddaaaa; font-style: italic;">ブースト</span>させよう
+                    <h3>
+                        "Paleo Boost"はパレオダイエッターたちが<br>
+                        進化し続けるための情報共有コミュニティです
+                    </h3>
                 </div>
             </div>
-        </h1>
-        <br>
-        <div class="row">
-            <div class="mx-auto">
-                <h3>
-                    "Paleo Boost"はパレオダイエッターたちが<br>
-                    進化し続けるための情報共有コミュニティです
-                </h3>
-            </div>
-        </div>
-        <div class="login-contents">
-            <div class="row">
-                <div class="mx-auto mb-3">
-                    <div class="float-left">
-                        <a class="btn btn-success" href="{{ route('register') }}">新規登録はこちら</a>
-                    </div>
-                    <div class="float-right">
-                        <form method="post" action="{{ route('login.guest') }}">
-                            @csrf
-                            <input type="hidden" name="email" value="guest@gmail.com">
-                            <input type="hidden" name="password" value="guest1234">
-                            <button type="submit" class="btn btn-info">ゲストログイン(期間限定)</button>
-                        </form>
+            <div class="login-contents">
+                <div class="row">
+                    <div class="mx-auto mb-2">
+                        <div class="float-left">
+                            <a class="btn btn-success" href="{{ route('register') }}">新規登録はこちら</a>
+                        </div>
+                        <div class="float-right">
+                            <form method="post" action="{{ route('login.guest') }}">
+                                @csrf
+                                <input type="hidden" name="email" value="guest@gmail.com">
+                                <input type="hidden" name="password" value="guest1234">
+                                <button type="submit" class="btn btn-info">ゲストログイン(期間限定)</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
+                @yield('login_form')
             </div>
-            @yield('login_form')
         </div>
     </div>
     <div class="container top-contens pt-3">
